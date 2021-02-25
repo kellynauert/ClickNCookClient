94781752980ecfe938e78803f7d49483ac6975a9
Kenneth Griggs <kenrgriggs@gmail.com>
2021-02-25 18:47:10 -0500

    adjusted allrecipes.js component

--- src/components/Allrecipes.js
+++ src/components/Allrecipes.js
@@ -1,51 +1,32 @@
-import React from "react";
-import { Table, Button } from "reactstrap";
+import React, {useEffect, useState} from 'react';
+import { Table, Button } from 'reactstrap';
 
-const AllRecipes = (props) => {
-  const workoutMapper = () => {
-    return props.workouts.map((workout, index) => {
-      return (
-        <tr key={index}>
-          <th scope="row">{workout.id}</th>
-          <td>{workout.result}</td>
-          <td>{workout.description}</td>
-          <td>{workout.definition}</td>
-          {/* <td>{workout.owner_id}</td> */}
-          <td>
-            <Button
-              color="warning"
-              onClick={() => {
-                props.editUpdateWorkout(workout);
-                props.updateOn();
-              }}
-            >
-              Update
-            </Button>
+function AllRecipes(props) {
+            
+    let [recipe, setRecipe] = useState('');
+            
+        useEffect(() => {
+            getAllRecipes();
+        }, []);
+            
+        function getAllRecipes() {
+            fetch(`http://localhost:3000/recipe/${recipe.id}` /* <-- revise this URL */, {
+                method: 'GET',
+                headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+        })
+        .then((res) => res.json())
+        .then((resipe) => {
+            setRecipe(recipe.recipes[0]);
+        });
+    }
 
-          </td>
-        </tr>
-      );
-    });
-  };
+    return (
+        <>
+        </>
+    )
+}
 
-  return (
-    <>
-      <h3>Workout History</h3>
-      <hr />
-      <Table striped>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Result</th>
-            <th>Description</th>
-            <th>Definition</th>
-            {/* <th>Owner_id</th> */}
-          </tr>
-        </thead>
-        <tbody>{workoutMapper()}</tbody>
-      </Table>
-    </>
-  );
-};
 
 export default AllRecipes;
