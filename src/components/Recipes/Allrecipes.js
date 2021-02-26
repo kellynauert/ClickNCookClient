cc1376da61bfaa6b607a2f2d002dcea6dbc6172c
Kenneth Griggs <kenrgriggs@gmail.com>
2021-02-25 21:41:17 -0500

    broken allrecipes component

--- src/components/Recipes/Allrecipes.js
+++ src/components/Recipes/Allrecipes.js
@@ -1,8 +1,22 @@
 import React, {useEffect, useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function AllRecipes(props) {
+function AllRecipes() {
             
-    let [recipe, setRecipe] = useState('');
+    let [recipe, setRecipe] = useState([]);
             
         useEffect(() => {
             getAllRecipes();
@@ -18,13 +32,51 @@ function AllRecipes(props) {
         .then((res) => res.json())
         .then((res) => {
             setRecipe(res.recipes);
-        });
+            console.log(recipe)
+        }); 
     }
 
-    return (
-        <>
-        {/* Some stuff */}
-        </>
+    const recipeMapper = () => {
+        return recipe.map((recipe, index) => {
+          return (
+            <Grid item md={4} key={index}>
+              <Card key={index}>
+                <Box display="flex">
+                  <Box>Views {recipe.views}</Box>
+                </Box>
+                <CardHeader
+                  title={recipe.recipe_name}
+                  subheader={recipe.category}
+                />
+                <CardContent>
+                  <Typography
+                    color="textSecondary"
+                    gutterBottom
+                    variant="h6"
+                    component="h5"
+                  >
+                    Directions:
+                    <br />
+                    {recipe.directions}
+                  </Typography>
+                  <Typography>
+                    Cook Time: {recipe.cook_time}
+                    <br />
+                    Serves: {recipe.servings}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            // <>
+            // </>
+          );
+        });
+      };
+
+       return(
+           <>
+            {recipeMapper()}
+           </>
     )
 }
 
