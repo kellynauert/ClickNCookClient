62012b45869ef31cf4591ac9d2c347086407b704
kellynauert <kellynauert@gmail.com>
2021-02-25 22:20:36 -0500

    fixed fetch issue on allrecipes

--- src/components/Recipes/Allrecipes.js
+++ src/components/Recipes/Allrecipes.js
@@ -1,84 +1,61 @@
-import React, {useEffect, useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import DeleteIcon from '@material-ui/icons/Delete';
+import React, { useEffect, useState } from 'react';
+import {
+	Card,
+	CardHeader,
+	CardContent,
+	Typography,
+	Box,
+	Grid,
+} from '@material-ui/core/';
 
 function AllRecipes() {
-            
-    let [recipe, setRecipe] = useState([]);
-            
-        useEffect(() => {
-            getAllRecipes();
-        }, []);
-            
-        function getAllRecipes() {
-            fetch(`http://localhost:3000/recipe/`, {
-                method: 'GET',
-                headers: new Headers({
-                'Content-Type': 'application/json',
-            }),
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            setRecipe(res.recipes);
-            console.log(recipe)
-        }); 
-    }
+	let [recipe, setRecipe] = useState([]);
 
-    const recipeMapper = () => {
-        return recipe.map((recipe, index) => {
-          return (
-            <Grid item md={4} key={index}>
-              <Card key={index}>
-                <Box display="flex">
-                  <Box>Views {recipe.views}</Box>
-                </Box>
-                <CardHeader
-                  title={recipe.recipe_name}
-                  subheader={recipe.category}
-                />
-                <CardContent>
-                  <Typography
-                    color="textSecondary"
-                    gutterBottom
-                    variant="h6"
-                    component="h5"
-                  >
-                    Directions:
-                    <br />
-                    {recipe.directions}
-                  </Typography>
-                  <Typography>
-                    Cook Time: {recipe.cook_time}
-                    <br />
-                    Serves: {recipe.servings}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            // <>
-            // </>
-          );
-        });
-      };
+	useEffect(() => {
+		getAllRecipes();
+		console.log(recipe);
+	}, []);
 
-       return(
-           <>
-            {recipeMapper()}
-           </>
-    )
+	function getAllRecipes() {
+		fetch(`http://localhost:3000/recipe/`, {
+			method: 'GET',
+			headers: new Headers({
+				'Content-Type': 'application/json',
+			}),
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				setRecipe(res);
+				console.log(recipe);
+			});
+	}
+
+	const RecipeMapper = () => {
+		return recipe.map((recipe, index) => {
+			return (
+				<Grid item md={4} key={index}>
+					<Card key={index}>
+						<Box display='flex'>
+							<Box>Views {recipe.views}</Box>
+						</Box>
+						<CardHeader
+							title={recipe.recipe_name}
+							subheader={recipe.category}
+						/>
+						<CardContent>
+							<Typography>
+								Cook Time: {recipe.cook_time}
+								<br />
+								Serves: {recipe.servings}
+							</Typography>
+						</CardContent>
+					</Card>
+				</Grid>
+			);
+		});
+	};
+
+	return <RecipeMapper />;
 }
-
 
 export default AllRecipes;
